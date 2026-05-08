b772952b942b7b8d60ed66c3bd0442011b937b37
brunoakd <brunocarrilakd@gmail.com>
2026-05-08 20:42:13 -0300

    Install prism-media

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ async function playAspen(voiceChannel) {
 
     return true;
 }
-client.once('ready', async () => {
+client.once('clientReady', async () => {
 
     console.log(`Bot conectado como ${client.user.tag}`);
    
